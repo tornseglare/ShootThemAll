66af5c9e600a910e71bd63403352af6985605610
Snorvarg <jonlennryd@yahoo.se>
2022-03-03 22:06:35 +0100

    loads and display an image.

---
+++ ShootThemAll/Content/readme.txt
@@ -0,0 +1,4 @@
+ShootThemAll.mgcb is used by the nuget MonoGame.Content.Builder.Task to automatically build the .xnb image files.
+
+1. The bin and obj folders are created in this folder. 
+2. The content of the folder Content\bin\DesktopGL\ShootThemAll are then copied to the bin\Debug\net6.0\Content folder.
--- ShootThemAll/ShootThemAllGame.cs
+++ ShootThemAll/ShootThemAllGame.cs
@@ -5,20 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ShootThemAll
 {
     public class ShootThemAllGame : Game
     {
-        GraphicsDeviceManager graphics;
+        readonly GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        // The tint of the image. This will also allow us to change the transparency.
+        Color color = Color.White;
 
         public ShootThemAllGame()
         {
             graphics = new GraphicsDeviceManager(this)
             {
-                PreferredBackBufferWidth = 1920,
-                PreferredBackBufferHeight = 1080
+                PreferredBackBufferWidth = 2560,
+                PreferredBackBufferHeight = 1440
             };
+        }
+
+        protected override void Initialize()
+        {
+            this.Content.RootDirectory = "Content";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Art.Load(Content);
+
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Green);
+
+            spriteBatch.Begin(SpriteSortMode.Texture, BlendState.Opaque);
+            spriteBatch.Draw(Art.Snail, Vector2.Zero, null, color, 0f, Vector2.Zero, 1f, 0, 0);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
         }
     }
 }
