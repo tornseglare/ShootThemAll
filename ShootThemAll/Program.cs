e7de831e9677b8943d55f01c842c8e37a828b32f
Snorvarg <jonlennryd@yahoo.se>
2022-03-03 19:26:35 +0100

    bare minimum, working example.

---
+++ ShootThemAll/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShootThemAll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using ShootThemAllGame theGame = new();
+            theGame.Run();
+        }
+    }
+}
---
+++ ShootThemAll/ShootThemAllGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace ShootThemAll
+{
+    public class ShootThemAllGame : Game
+    {
+        GraphicsDeviceManager graphics;
+
+        public ShootThemAllGame()
+        {
+            graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 1920,
+                PreferredBackBufferHeight = 1080
+            };
+        }
+    }
+}
