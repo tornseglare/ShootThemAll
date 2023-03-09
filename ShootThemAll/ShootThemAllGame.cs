b70fd54e6a3b3cf77874b6d11fa81dc6602434e0
Snorvarg <jonlennryd@yahoo.se>
2023-03-09 16:21:36 +0100

    The 'camera' is now travelling between the stars.

--- ShootThemAll/ShootThemAllGame.cs
+++ ShootThemAll/ShootThemAllGame.cs
@@ -19,18 +19,19 @@ namespace ShootThemAll
 
     public class ShootThemAllGame : Game
     {
+        // Try to set these to false to see the movement between the stars more clearly.
+        bool rotate = true;
+        bool zoomInAndOut = true;  
+
         readonly GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
-        // Set to true to draw the nice flower instead of the galaxy.
-        readonly bool experimentalFlower = false;
 
         readonly int windowedWidth = 1000;
         readonly int windowedHeight = 1000;
         readonly Random randomizer = new();
 
-        float flowerScale = 2.5f;
-        float growShrink = 1.0f;
+        float scale = 3.0f;
+        double growShrink = 1.0d;
 
         readonly List<PositionTexture> signPositions = new();
         Color signColor = new(255, 255, 255, 210);
@@ -38,6 +39,12 @@ namespace ShootThemAll
         float mapRot = 0.0f;
         Color mapColor = new(0, 255, 255, 255);
         Vector2 mapCenter;
+
+        // The camera is moving between the stars. 
+        Vector2 cameraCenter;
+
+        // Which star is the camera moving towards? 
+        int travellingTo = 0;
 
         public ShootThemAllGame()
         {
@@ -96,6 +103,8 @@ namespace ShootThemAll
             signPositions.Add(new PositionTexture() { position = new(randomizer.Next(-w, w), randomizer.Next(-h, h)), texture = Art.Corvus , center = signCenter });
             signPositions.Add(new PositionTexture() { position = new(randomizer.Next(-w, w), randomizer.Next(-h, h)), texture = Art.Grus , center = signCenter });
 
+            cameraCenter = signPositions[travellingTo].position;
+
             base.LoadContent();
         }
 
@@ -103,24 +112,18 @@ namespace ShootThemAll
         {
             base.Update(gameTime);
 
-            mapRot += 0.01f;
+            if(rotate)
+                mapRot += 0.01f;
 
-            if(experimentalFlower)
-                UpdateFlower(gameTime);
+            if(zoomInAndOut)
+                ZoomInAndOut(gameTime);
+
+            TravelToStar(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            if (experimentalFlower)
-            {
-                DrawFlower();
-                DrawCircleAndAxis();
-            }
-            else
-            {
-                DrawRotatingMap();
-            }
-
+            DrawRotatingMap();
             base.Draw(gameTime);
         }
 
@@ -128,117 +131,128 @@ namespace ShootThemAll
         {
             GraphicsDevice.Clear(Color.Black);
 
+            Matrix CameraMatrix = Matrix.CreateTranslation(-cameraCenter.X,-cameraCenter.Y, 0);
             Matrix RotMatrix = Matrix.CreateRotationZ(mapRot);
-            Matrix ScaleMatrix = Matrix.CreateScale(0.8f);
+            Matrix ScaleMatrix = Matrix.CreateScale(scale);
             Matrix PosMatrix = Matrix.CreateTranslation(500, 500, 0);
 
             // The multiplication order is important when working with Matrixes, if you change the order you get entire different results. 
             // 
-            // RotMatrix * ScaleMatrix * PosMatrix can be translated into: 
-            // * Rotate the 'world'. (all images are rotated around 0,0)
+            // CameraMatrix * RotMatrix * ScaleMatrix * PosMatrix can be translated into: 
+            // * Move the world to where the camera is.
+            // * Rotate the 'world'. (all images are rotated around the camera's X,Y position)
             // * Scale the world. (scaling down all the images)
             // * Move the world 500,500 pixels. (This is not scaled down as we put it last in the multiplication)
             // 
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied, null, null, null, null, RotMatrix * ScaleMatrix * PosMatrix);
+            spriteBatch.Begin(
+                SpriteSortMode.Deferred, 
+                BlendState.NonPremultiplied, 
+                null, null, null, null,
+                CameraMatrix * RotMatrix * ScaleMatrix * PosMatrix);
 
             // The map image is rotated around its own center.
-            spriteBatch.Draw(Art.StarMap, Vector2.Zero, null, mapColor, 0, mapCenter, 1.0f, 0, 0);
+            spriteBatch.Draw(
+                Art.StarMap, 
+                Vector2.Zero, 
+                null, mapColor, 0,
+                mapCenter, 
+                1.0f, 0, 0);
 
-            // The map icons follows the map rotation. To make it look more google-maps alike their images are not rotated but kept straight.
+            // The map icons are positioned around the map center, so follows the map rotation.
+            // To make it look more google-maps alike their images are not rotated but kept straight.
             foreach (PositionTexture pt in signPositions)
             {
                 // We 'undo' the rotation of the images by subtracting the angle here. If you leave angle at zero the images will be rotated too.
-                spriteBatch.Draw(pt.texture, pt.position, null, signColor, -mapRot, pt.center, 0.6f, 0, 0);
+                spriteBatch.Draw(
+                    pt.texture, 
+                    pt.position, 
+                    null, signColor, -mapRot, 
+                    pt.center, 
+                    0.6f, 0, 0);
             }
+
+            spriteBatch.End();
+
+            // Draw the star we are travelling to at the top-left corner of the view.
+            spriteBatch.Begin(
+                SpriteSortMode.Deferred,
+                BlendState.NonPremultiplied);
+
+            PositionTexture targetStar = signPositions[travellingTo];
+            spriteBatch.Draw(
+                targetStar.texture,
+                Vector2.Zero,
+                null, signColor, 0,
+                Vector2.Zero,
+                0.4f, 0, 0);
 
             spriteBatch.End();
         }
 
-        private void UpdateFlower(GameTime gameTime)
+        private void TravelToStar(GameTime gameTime)
         {
-            flowerScale -= 0.01f;
+            Vector2 target = signPositions[travellingTo].position;
+
+            if(CloseEnough(cameraCenter.X, target.X) && CloseEnough(cameraCenter.Y, target.Y))
+            {
+                // We found our star. Travel to the next one.
+                travellingTo++;
+                if(travellingTo >= signPositions.Count)
+                {
+                    travellingTo = 0;
+                }
+            }
+            else
+            {
+                Vector2 diff = cameraCenter - target;
+
+                cameraCenter.X -= Move(diff.X, gameTime);
+                cameraCenter.Y -= Move(diff.Y, gameTime);
+            }
+        }
+
+        // Move with the minimum speed of 0.01f.
+        private static float Move(float along, GameTime gameTime)
+        {
+            float speed = 3.0f;
+            float movement = (float)(along / (speed * gameTime.ElapsedGameTime.TotalMilliseconds));
+
+            if (along > 0 && movement < 0.02f)
+                movement = 0.02f;
+            if (along < 0 && movement > -0.02f)
+                movement = -0.02f;
+
+            return movement;
+        }
+
+        private static bool CloseEnough(float pos1, float pos2)
+        {
+            if(pos1 + 0.05f  > pos2 && pos1 - 0.05f < pos2) 
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void ZoomInAndOut(GameTime gameTime)
+        {
+            double speed = 800; // Larger is slower.
+            float maxScale = 5.0f;
 
             // Let the scale grow and shrink over time.
-            flowerScale += growShrink * (float)gameTime.ElapsedGameTime.TotalMilliseconds / 99;
+            scale += (float)(growShrink * gameTime.ElapsedGameTime.TotalMilliseconds / speed);
 
-            if (flowerScale > 2.0f)
+            if (scale > maxScale)
             {
                 growShrink = -1.0f;
-                flowerScale = 2.0f;
+                scale = maxScale;
             }
-            else if (flowerScale < 0.1f)
+            else if (scale < 0.3f)
             {
                 growShrink = 1.0f;
-                flowerScale = 0.1f;
+                scale = 0.3f;
             }
-        }
-
-        // Draw a circle, but change the radius while drawing it. Turns into a flower. :-) 
-        private void DrawFlower()
-        {
-            // First we create a position as a matrix. This is pixel position.
-            // Using Matrix requires three dimensions, XYZ. We set Z to zero since we are a 2Dgame.
-            Matrix positionMatrix = Matrix.CreateTranslation(300.0f, 300.0f, 0.0f);
-
-            // Second we create the rotation matrix. The best way to see this is as one of the hands on the clock. I call it stick. 
-            // We just give the stick a length in pixels, and this will be rotated, so effectively it becomes the radius. 
-            // Idea: The stick is rotated angle radians, with the skull at the end of the stick. It will look like the skull is rotating 
-            // around the clock, i.e it is rotating around a center. 
-            Vector3 stick = new(80, 0, 0);
-
-            Matrix rotationMatrix = Matrix.CreateTranslation(stick);
-
-            // Rotate the matrix.
-            rotationMatrix *= Matrix.CreateRotationZ(mapRot);
-
-            // Scale the rotated stick. The scale matrix is scaling all three dimensions, but we use only x and y.
-            Matrix scaleMatrix = Matrix.CreateScale(flowerScale);
-            rotationMatrix *= scaleMatrix;
-
-            positionMatrix += rotationMatrix;
-
-            // Multiply a 1,1,1 vector with the matrix, to get the xyz position. 
-            Vector3 v3position = Vector3.Transform(Vector3.One, positionMatrix);
-
-            // spriteBatch.Draw() must have a Vector2 so transfer the coordinates.
-            Vector2 xStickPosition = new(v3position.X + randomizer.NextSingle() / 1.0f, v3position.Y + randomizer.NextSingle() / 1.0f);
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
-            spriteBatch.Draw(Art.Pixel, xStickPosition, Color.White);
-            spriteBatch.End();
-        }
-
-        // Draw 4 pixels showing the axis movements.
-        private void DrawCircleAndAxis()
-        {
-            Matrix positionMatrix = Matrix.CreateTranslation(150.0f, 150.0f, 0.0f);
-            Vector3 v3position = Vector3.Transform(Vector3.One, positionMatrix);
-            Vector2 origPosition = new(v3position.X, v3position.Y);
-
-            Matrix rotationMatrix = Matrix.CreateTranslation(new(80, 0, 0));
-            rotationMatrix *= Matrix.CreateRotationY(mapRot);
-            v3position = Vector3.Transform(Vector3.One, positionMatrix + rotationMatrix);
-            Vector2 xStickPosition = new(v3position.X, v3position.Y);
-
-            rotationMatrix = Matrix.CreateTranslation(new(0, 80, 0));
-            rotationMatrix *= Matrix.CreateRotationX(mapRot - (float)Math.PI / 2);
-            v3position = Vector3.Transform(Vector3.One, positionMatrix + rotationMatrix);
-            Vector2 yStickPosition = new(v3position.X, v3position.Y);
-
-            rotationMatrix = Matrix.CreateTranslation(new(80, 0, 0));
-            rotationMatrix *= Matrix.CreateRotationZ(mapRot);
-            v3position = Vector3.Transform(Vector3.One, positionMatrix + rotationMatrix);
-            Vector2 zStickPosition = new(v3position.X, v3position.Y);
-
-            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
-
-            // Decided to draw a single pixel to make the movement pattern clearer than using a fuzzy skull. Replace with Skull if its too tiny.
-            spriteBatch.Draw(Art.Pixel, origPosition, Color.White);
-            spriteBatch.Draw(Art.Pixel, xStickPosition, Color.Blue);
-            spriteBatch.Draw(Art.Pixel, yStickPosition, Color.Red);
-            spriteBatch.Draw(Art.Pixel, zStickPosition, Color.Green);
-
-            spriteBatch.End();
         }
     }
 }
